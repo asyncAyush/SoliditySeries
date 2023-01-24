60f06b3e46ed0c340d537aeb9bd236eaaefbc5d5
Ayushkumar95 <101060011+Ayushkumar95@users.noreply.github.com>
2023-01-24 20:54:19 +0530

    contact local

---
+++ second.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: GLP-3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract local
+{
+    function store() pure public returns(uint)
+    {
+        uint age=10;
+        return age;
+    }
+}
