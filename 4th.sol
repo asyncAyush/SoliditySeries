e8d23f118596fb84a0777217de8dc52e7d85b76e
Ayushkumar95 <101060011+Ayushkumar95@users.noreply.github.com>
2023-01-26 20:45:04 +0530

    added 4th.sol

---
+++ 4th.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: GLP-3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+contract local{
+    uint age= 10;
+    function getter() public view returns(uint)
+    {
+        return age;
+    }
+    function setter(uint newage) public 
+    {
+        age = newage;
+    }
+}
