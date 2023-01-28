4a911ae44206d26a8325aba5d840653be7094314
Ayushkumar95 <101060011+Ayushkumar95@users.noreply.github.com>
2023-01-28 15:06:55 +0530

    adding state3.sol

---
+++ 6th state3.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: GLP-3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract local
+{
+uint public  count;
+ 
+ constructor( uint new_count)
+ {
+     count = new_count;
+ }
+}
