6727d1de58cbff8a4d9b8138de3b9d6ff54993a2
Ayushkumar95 <101060011+Ayushkumar95@users.noreply.github.com>
2023-02-12 09:14:36 -0500

    adding global variable

---
+++ 17th global variable.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: GLP-3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract demo
+{
+    function getter() public view returns (uint block_no,uint timestamp, address msgsender)
+    {
+        return (block.number,block.timestamp,msg.sender);
+    }
+}
