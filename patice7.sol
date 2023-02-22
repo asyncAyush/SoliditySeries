f5cd934c14d66727d6c479128f1d37751304d1ed
Ayushkumar95 <101060011+Ayushkumar95@users.noreply.github.com>
2023-02-22 08:40:16 -0500

    Update patice7.sol

--- patice7.sol
+++ patice7.sol
@@ -1,1 +1,11 @@
+//SPDX-License-Identifier: GLP-3.0
 
+pragma solidity  0.5.0 ;
+
+contract local
+{
+    uint public money = 255;
+     function setter() public{
+         money =money+1;
+     }
+}
