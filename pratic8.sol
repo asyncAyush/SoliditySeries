8bdb5ef935316d6f52e86ed2a6970e7c57d63747
Ayushkumar95 <101060011+Ayushkumar95@users.noreply.github.com>
2023-02-24 08:42:23 -0500

    adding pratice8.sol

---
+++ pratic8.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: GLP-3.0
+
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract Array
+{
+    uint[4] public arr = [20, 60, 70,80];
+
+    function setter(uint index,uint value) public
+    {
+        arr[index]=value;
+    }
+    function length() public
+    {
+        arr.length;
+    }
+    
+
+}
