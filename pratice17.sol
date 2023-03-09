fc6e1a00c24e9d4d28a6a1ba5194ca7dade19ed0
Ayushkumar95 <101060011+Ayushkumar95@users.noreply.github.com>
2023-03-08 16:53:37 -1100

    adding pratice17.sol

--- pratice17.sol
+++ pratice17.sol
@@ -1,1 +1,13 @@
+//SPDX-License-Identifier: GLP-3.0
 
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract state
+{
+    mapping (uint => string) public roll_no;
+    function setter(uint roll, string memory name) public
+    {
+        roll_no[roll]=name;
+    }
+
+}
