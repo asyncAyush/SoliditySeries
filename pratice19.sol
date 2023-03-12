b6518803616181fd3080dfed887351d2f1de5e26
Ayushkumar95 <101060011+Ayushkumar95@users.noreply.github.com>
2023-03-12 03:09:25 -0400

    adding pratice19.sol

--- pratice19.sol
+++ pratice19.sol
@@ -1,1 +1,17 @@
+//SPDX-License-Identifier: GLP-3.0
 
+pragma solidity >= 0.5.0 < 0.9.0;
+
+contract demo
+{
+    string[] public student = ['aman','rita','ravi'];
+    function mem() public{
+        string[] memory s1=student;
+        s1[0]='akash';
+    }
+    function sto()public{
+        string[] storage s1=student;
+        s1[0]='akash';
+
+    }
+}
