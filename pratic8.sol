dced4c1b1406dd51d9278e0d8649ced0ef361cae
Ayush Kumar <101060011+asyncAyush@users.noreply.github.com>
2023-03-31 08:42:34 -0400

    Update pratic8.sol

--- pratic8.sol
+++ pratic8.sol
@@ -4,7 +4,7 @@ pragma solidity >= 0.5.0 < 0.9.0;
 
 contract Array
 {
-    uint[4] public arr = [20, 60, 70,80];
+  uint[4] public arr = [20, 60, 70,80];
 
     function setter(uint index,uint value) public
     {
